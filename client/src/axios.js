ce21113234cddb2a690d06b73153c105092e8b96
Praneeth2k <bpraneeth2000@gmail.com>
2021-12-14 15:31:50 +0530

    .

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 
-const baseURL = process.env.baseURL || "http://localhost:8001"
+const baseURL = process.env.REACT_APP_BASE_URL
 const instance = axios.create({
     baseURL
 })
