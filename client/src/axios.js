fa3eb854e0929d1653047f1e416c1c2818b60f33
Praneeth2k <bpraneeth2000@gmail.com>
2021-12-14 16:33:01 +0530

    fixed bug in server.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 
-const baseURL = process.env.REACT_APP_BASE_URL
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:8001"
 const instance = axios.create({
     baseURL
 })
--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('/user', userRoutes)
 app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", index.html))
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
 app.listen(port, ()=>console.log(`listening on localhost: ${port}`))
