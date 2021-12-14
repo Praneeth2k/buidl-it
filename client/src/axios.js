fde15ad0dd42bd74687e17ee5d3af1c8bc71f48a
Praneeth2k <bpraneeth2000@gmail.com>
2021-12-14 13:54:24 +0530

    changed axios baseURL

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+
+
+const baseURL = process.env.baseURL || "http://localhost:8001"
 const instance = axios.create({
-    baseURL: 'http://localhost:8001'
+    baseURL
 })
 
 export default instance
